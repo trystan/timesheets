f818dff8c9ddea4117f4643112ec307bdba9ad77
Trystan <trystan.s@comcast.net>
2023-11-14 08:39:01 -0800

    Add jsonschema validation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,4 +1,6 @@
 import express from 'express'
+import { validate } from 'express-jsonschema'
+import { JSONSchema4 } from 'json-schema'
 
 const app = express()
 const port = process.env.PORT ?? 8080
@@ -28,13 +30,31 @@ app.get('/timesheets', (req, res) => {
   res.json(timesheets);
 });
 
-app.post('/timesheets', (req, res) => {
+const TimesheetSchema: JSONSchema4 = {
+  type: 'object',
+  properties: {
+    name: { type: 'string', required: true },
+    description: { type: 'string', required: true },
+    rate: { type: 'number', required: true },
+    items: {
+      type: "array",
+      items: {
+        properties: {
+          date: { type: 'string', format: 'date', required: true },
+          numberOfMinutes: { type: 'number', required: true }
+        },
+      }
+    }
+  }
+}
+
+app.post('/timesheets', validate({ body: TimesheetSchema }), (req, res) => {
   const timesheet = { id: timesheets.length, ...req.body }
   timesheets.push(timesheet)
   res.sendStatus(201)
 });
 
-app.put('/timesheets/:id', (req, res) => {
+app.put('/timesheets/:id', validate({ body: TimesheetSchema }), (req, res) => {
   const id = parseInt(req.params.id, 10)
   const updatedTimesheet = { ...req.body }
   const index = timesheets.findIndex(t => t.id === id)
@@ -50,6 +70,30 @@ app.get('/timesheets/:id', (req, res) => {
   const id = parseInt(req.params.id, 10)
   res.json(timesheets.find(t => t.id === id));
 });
+
+app.use((err: any, req: any, res: any, next: any) => {
+ 
+  var responseData;
+
+  if (err.name === 'JsonSchemaValidation') {
+      console.log(err.message);
+      res.status(400);
+
+      responseData = {
+         statusText: 'Bad Request',
+         jsonSchemaValidation: true,
+         validations: err.validations  // All of your validation information
+      };
+
+      if (req.xhr || req.get('Content-Type') === 'application/json') {
+          res.json(responseData);
+      } else {
+          res.render('badrequestTemplate', responseData)
+      }
+  } else {
+      next(err)
+  }
+})
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
